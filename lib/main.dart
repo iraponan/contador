5f5d9e39336c9ff9dc7b52ffff2ca16f068997e8
Iraponan Marinho <iraponan.aires@gmail.com>
2024-01-18 23:35:11 -0300

    Definindo o design da tela.

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
@@ -30,50 +31,84 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.red,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Pode Entrar!',
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.white,
-                fontWeight: FontWeight.w700,
-              ),
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/image.jpg'),
+              fit: BoxFit.cover,
             ),
-            const Text(
-              '0',
-              style: TextStyle(
-                fontSize: 100,
-                color: Colors.white,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Pode Entrar!',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: decrement,
-                  child: Text(
-                    'Saiu',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                    ),
+              const Padding(
+                padding: EdgeInsets.all(40),
+                child: Text(
+                  '0',
+                  style: TextStyle(
+                    fontSize: 100,
+                    color: Colors.white,
                   ),
                 ),
-                TextButton(
-                  onPressed: increment,
-                  child: Text(
-                    'Entrou',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: decrement,
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      fixedSize: const Size(100, 100),
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          24,
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      'Saiu',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  const SizedBox(
+                    width: 32,
+                  ),
+                  TextButton(
+                    onPressed: increment,
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      fixedSize: const Size(100, 100),
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          24,
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      'Entrou',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ));
   }
 }
