3781cceb93bf7bccc4acc8db0fc2cd0699e0b8ef
Iraponan Marinho <iraponan.aires@gmail.com>
2024-01-18 22:41:16 -0300

    Linha e Botão.

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,22 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void decrement() {
+    print('Decrement');
+  }
+
+  void increment() {
+    print('Increment');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        backgroundColor: Colors.black,
+    return Scaffold(
+        backgroundColor: Colors.red,
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'Pode Entrar!',
               style: TextStyle(
                 fontSize: 30,
@@ -33,13 +41,38 @@ class HomePage extends StatelessWidget {
                 fontWeight: FontWeight.w700,
               ),
             ),
-            Text(
+            const Text(
               '0',
               style: TextStyle(
                 fontSize: 100,
                 color: Colors.white,
               ),
-            )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: decrement,
+                  child: Text(
+                    'Saiu',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: increment,
+                  child: Text(
+                    'Entrou',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ));
   }
